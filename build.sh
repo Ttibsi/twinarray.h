d1589d7bf106e5504cdfd1cf6cd80645262571b0
ttibsi <ashisbitt@icloud.com>
2024-12-19 16:54:17 +0000

    setup unit testing with boost::ut

---
+++ build.sh
@@ -0,0 +1,3 @@
+
+time clang++ -std=c++20 -Wall -Wextra -g main.cpp -o test
+./test
--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include "twin_array.h"
 
-int main() {
-    auto buf = TwinArray<char>();
-}
+#define BOOST_UT_DISABLE_MODULE
+#include "ut.hpp"
+
+namespace ut = boost::ut;
+
+ut::suite<"constructors"> constructors = [] {
+    using namespace ut;
+
+    "empty_constructor"_test = [] {
+        auto buf = TwinArray<int>();
+        expect(true) << "hello";
+    };
+};
+
+int main() {}
